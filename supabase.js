69289b85f8eb7aa106d47377043c9761e2739ae0
silvaxtz <psilvaa149@gmail.com>
2026-07-28 17:26:42 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 const SUPABASE_URL = "https://bmrlpwohiakwiamikxci.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_SaFWPBCTvr6iQ_EJ4iSK8Q_Xfmui4oC";
 
-const supabase = window.supabase.createClient(
+const db = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
