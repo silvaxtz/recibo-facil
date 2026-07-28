f6771667ceebbc9ac88c02b378b1deedbd3610c5
silvaxtz <psilvaa149@gmail.com>
2026-07-28 16:54:56 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://bmrlpwohiakwiamikxci.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_SaFWPBCTvr6iQ_EJ4iSK8Q_Xfmui4oC";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
